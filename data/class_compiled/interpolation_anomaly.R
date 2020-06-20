cb06d83ca281ebacb28ab648c8e5a1386328e95a
yeshancqcq <yeshan@umich.edu>
2020-06-20 11:12:08 -0500

    Create interpolation_anomaly.R

---
+++ data/class_compiled/interpolation_anomaly.R
@@ -0,0 +1,86 @@
+library(plyr)
+library(astrochron)
+library(stats)
+
+metadata <- read_csv("data/metadata.csv")
+temp_data <- metadata
+anomaly_data <- metadata
+
+files <- list.files(path="lipd2csv_data", 
+                    pattern="*.csv", full.names=TRUE, recursive=FALSE)
+
+
+lapply(files, function(x) {
+  data <- read.csv(x, skip=0)
+  filename <- basename(x)
+  
+  # selecting only relevant columns
+  dt <- data.frame(age = data$age, temperature = data$temperature)
+  
+  # removing all na
+  dt <- na.omit(dt)
+  
+  # sorting by age (in LipD there are unsorted data)
+  dt <- dt[order( dt[,1] ),]
+  
+  cat("processing", filename)
+  
+  start_age = dt$age[1]
+  
+  # Getting the next 100 year point prior to the latest age
+  start_num = round_any(start_age, 100, f = ceiling) 
+  
+  # Doing the linear interpolation with 100-yr steps
+  # Starting at the next 100 year point prior to the latest age
+  dt_int=linterp(dt, dt=100, start=start_num, genplot=F)
+  
+  # get the average between 8000 and 12000 bp as reference (anomaly base) 
+  anomaly_base = NA
+  counter <- 0
+  sumRange <- 0
+  
+  for (i in 1:nrow(dt_int)){
+    if(dt_int$age[i]>=8000 && dt_int$age[i]<= 12000){
+      if(dt_int$temperature[i]!=0){
+        sumRange <- sumRange + dt_int$temperature[i]
+        counter <- counter + 1
+      }
+    }
+  }
+  
+  # if there are more than 10 data points within the 8000-12000 bp range, we regard it a valid case for generating the anomaly base
+  if(counter >= 10){
+    anomaly_base <- sumRange/counter
+  } else {
+    anomaly_base = NA
+  }
+  
+  # initiating 2 vectors, one for temperature anomaly, one for abandoned 
+  # files (those without data at 10000 years bp)
+  abandoned_files <- vector()
+  temp_anomaly <- vector()
+  
+  # Check if it has data for anomaly years bp
+  # if yes: generate the anomaly vector
+  # if no: print the filename and add it to the abandoned files
+  if (is.na(anomaly_base)){
+    cat(filename," does not contain the anomaly standard.")
+    abandoned_files <- c(abandoned_files, filename)
+  } else {
+    # Generating the new data frame with age, temp and anomaly columns
+    dt_df <- data.frame(Age=seq.int(0,22000,100), Temperature=NA, Anomaly=NA)
+    for(i in 1:nrow(dt_df)){
+      for(j in 1:nrow(dt_int)){
+        if(dt_int$age[j] == dt_df$Age[i]){
+          dt_df$Temperature[i] <- dt_int$temp[j]
+          dt_df$Anomaly[i] <- dt_int$temp[j] -anomaly_base
+        }
+      }
+    }
+    
+    # Writing the csv
+    write.csv(dt_df, paste0("anomaly_interpolated/", filename), row.names=F)
+
+
+  }
+})
